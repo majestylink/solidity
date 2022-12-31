29ca9c7983f797a9e7e2da6e6ad992d5f1ebcb99
majestylink <atuhsamuel96@gmail.com>
2022-12-31 11:48:32 +0100

    Text message

---
+++ contracts/BikeChain.sol
@@ -0,0 +1,25 @@
+// SPDX-License-Identifier: Unlicensed
+
+pragma solidity ^0.8.7;
+
+contract BikeChain {
+
+    address owner;
+
+    constructor() {
+        owner = msg.sender;
+    }
+    // Add youself as a renter
+
+    // Checkout bike
+
+    // Check in a bike
+
+    // Get total duration of bike use
+
+    // Get contract balance
+
+    // Get rnter's balance
+
+    // Set due amount
+}
